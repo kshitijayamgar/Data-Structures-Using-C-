8dac257b47ac6ea47b9f9b9fd0f35b1e95ac40a5
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-26 14:03:19 +0530

    prg03.c

--- Recursion/prg03.c
+++ Recursion/prg03.c
@@ -7,10 +7,4 @@ void fun(){
 void main(){
 	fun();
 }
-/* 
-  ha chukicha prg ahe ithe global var use kelay he check karnyasthi ki maagchya prg mde static var use kela hota
-  static variable  aani global variable  donhi variable data section vr store hotat,
-  pan asa nemka kay farak pdto he bagmyasathi ha prg 
-  nit lksh deel tr hya prg compilation  ntr as o/p kiti stack frame push hotat he imp ahe tyane 
-  hya donhi variable mdhla farak farak clear hoil..
- result =300 kiva 200 stack frame kmi hotat jeva global var use krto  */
+//program with runtime error segmetation fault
