6f617861a420f230859fc88618c687aefb1ef605
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-26 13:56:57 +0530

    prg04.c

---
+++ Recursion/prg04.c
@@ -0,0 +1,12 @@
+/*ha ek simple prg ahe jyne 1-10 number print hot ahet hach prg aplyala Recursion mde convert krych ahe*/
+#include<stdio.h>
+void fun(int x){
+for(int i=1;i<=x;i++){
+	printf("%d\n",i);
+	
+    }
+  printf("\n");
+}
+void main(){
+	fun(5);
+}
