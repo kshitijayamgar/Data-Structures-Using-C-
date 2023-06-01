17e04cfc30ea624a737b4ea8298ec48c6bca1cc6
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-06-01 12:39:22 +0530

    program for print natural numbers using Recursion

--- prg21.c
+++ prg21.c
@@ -3,8 +3,7 @@ int  numPrint(int);
 int main()
 {
     int n = 1;
-	//printf("\n\n Recursion : print first 50 natural numbers :\n");
-	//printf("-------------------------------------------------\n"); 
+
 	printf(" The natural numbers are :");
     numPrint(n);
     printf("\n\n");
